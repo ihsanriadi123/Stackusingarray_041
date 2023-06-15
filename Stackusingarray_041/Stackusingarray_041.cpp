52156bb06010147e236537c1adada256fb7941c0
ihsan putra riadi <iputrariadi@gmail.com>
2023-06-15 10:36:57 +0700

    Menggabungkan kata "stack" dan "program" untuk menunjukkan bahwa ini adalah implementasi stack dalam program

--- Stackusingarray_041/Stackusingarray_041.cpp
+++ Stackusingarray_041/Stackusingarray_041.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 class program {
 private:
-	
+	string stack_array[5];
+	int top;
+
+public:
+	program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "stack full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << " ditambahkan(pushed). " << endl;
+
+		return element;
+	}
 
 };
